3a4e71d890075c609eb92548b834e9f8a10fd3b4
MrLuit <me@luithollander.nl>
2017-05-20 12:00:52 +0200

    Einde toegevoegd

--- js/script.js
+++ js/script.js
@@ -13,6 +13,55 @@ function shuffle(array) {
     }
     return array;
 }
+
+function connectToMagister() {
+    $.get("https://lu1t.nl/magister.php?username=" + encodeURIComponent($("#username").val()) + "&password=" + encodeURIComponent($("#password").val()), function(data) {
+            userinfo = JSON.parse(data);
+            if (userinfo.error) {
+                $("#magister").fadeOut("", function() {
+                    $("#error").fadeIn();
+                });
+            } else if (userinfo.success && !userinfo.error) {
+                $("#magister").fadeOut("", function() {
+                    $("#vraag1").fadeIn();
+                });
+                userinfo.vakken = shuffle(userinfo.vakken);
+                $.each(userinfo.vakken, function(i, vak) {
+                    if ($.inArray(vak.afkorting, ["me", "lo", "rt3F", "MS", "mvt"]) === -1 && vak.afkorting.indexOf("sl_") === -1 && vak.afkorting.indexOf("co_") === -1) { // WISA / WISAC        LO, HV, TE, MU, DR?
+                        $(".vakkenmoeite").append("<p><input type='checkbox' id='" + vak.afkorting + "' /><label for='" + vak.afkorting + "'>" + vak.omschrijving + "</label></p>");
+                    }
+                });
+            } else {
+                setTimeout(function() {
+                    $(".magisterspinner").fadeOut("", function() {
+                        $(".section.magister").fadeIn();
+                        Materialize.toast('Verkeerde gebruikersnaam of wachtwoord!', 4000)
+                    });
+                }, 1000);
+            }
+        })
+        .fail(function() {
+            setTimeout(function() {
+                $(".magisterspinner").fadeOut("", function() {
+                    $(".section.magister").fadeIn();
+                    Materialize.toast('Geen internetverbinding', 4000)
+                });
+            }, 1000);
+        })
+}
+
+function finishUp() {
+    $.get("https://lu1t.nl/enquete.php?data=" + encodeURIComponent(JSON.stringify(antwoorden)), function(data) {
+            $("#vraag1").fadeOut("", function() {
+                $("#einde").fadeIn();
+            });
+        })
+        .fail(function() {
+            setTimeout(function() {
+                Materialize.toast('Geen internetverbinding', 4000)
+            }, 1000);
+        })
+}
 $(document).ready(function() {
     $(window).keydown(function(event) {
         if (event.keyCode == 13) {
@@ -29,33 +78,9 @@ $(document).ready(function() {
         $(".section.magister").fadeOut("", function() {
             $(".magisterspinner").fadeIn();
         });
-        $.get("https://luithollander.nl/magister/enquete_magister_api.php?username=" + encodeURIComponent($("#username").val()) + "&password=" + encodeURIComponent($("#password").val()), function(data) {
-                userinfo = JSON.parse(data);
-                if (userinfo.error) {
-                    $("#magister").fadeOut("", function() {
-                        $("#error").fadeIn();
-                    });
-                } else if (userinfo.success && !userinfo.error) {
-                    $("#magister").fadeOut("", function() {
-                        $("#vraag1").fadeIn();
-                    });
-                    userinfo.vakken = shuffle(userinfo.vakken);
-                    $.each(userinfo.vakken, function(i, vak) {
-                        if ($.inArray(vak.afkorting, ["me", "lo", "rt3F", "MS", "mvt"]) === -1 && vak.afkorting.indexOf("sl_") === -1 && vak.afkorting.indexOf("co_") === -1) { // WISA / WISAC        LO, HV, TE, MU, DR?
-                            $(".vakkenmoeite").append("<p><input type='checkbox' id='" + vak.afkorting + "' /><label for='" + vak.afkorting + "'>" + vak.omschrijving + "</label></p>");
-                        }
-                    });
-                } else {
-                    setTimeout(function() {
-                        $(".magisterspinner").fadeOut("", function() {
-                            $(".section.magister").fadeIn();
-                            Materialize.toast('Verkeerde gebruikersnaam of wachtwoord!', 4000)
-                        });
-                    }, 1000);
-                }
-            })
-            .fail(function() {
-                Materialize.toast('Geen internetverbinding', 4000)
-            })
+        connectToMagister();
+    });
+    $("#klaar").click(function() {
+        finishUp();
     });
 });
